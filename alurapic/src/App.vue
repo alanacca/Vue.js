7113bca8de7d4a02fd1f40b874311ed073e8bf9e
alanacca <alana.araujo35@gmail.com>
2021-08-27 14:00:49 -0300

    Mostrar Imagens da API

---
+++ alurapic/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{titulo}}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: "AluraPic",
+
+      fotos: []
+    }
+  },
+
+  created() {
+    let promise = this.$http.get('http://localhost:3000/v1/fotos')
+      .then(res => res.json())//É pego a respota da API no seu formato json
+      .then(fotos => this.fotos = fotos, err => console.log(err));//São pegas as fotos da API e colocadas na variavel do export(this.fotos)
+  }
+
+}
+</script>
+
+<style>
+
+</style>
