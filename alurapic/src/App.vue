4749364905d003b8cd3c96874f9d8622dd290171
alanacca <alana.araujo35@gmail.com>
2021-08-30 13:48:33 -0300

    Adicionando filtro e comportamento ao componente

--- alurapic/src/App.vue
+++ alurapic/src/App.vue
@@ -1,22 +1,55 @@
+
 <template>
-  <div>
-    <h1>{{titulo}}</h1>
-    <ul>
-      <li v-for="foto of fotos">
-        <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+    <h1 class="centralizado">{{titulo}}</h1>
+    
+    <input type="search" class="filtro" @input="filtro= $event.target.value" placeholder="filtre por parte do título">
+    <!--o data binding, v-on = @, vai indicar que no evento de input ele vai colocar a variavel filtro do data igual ao que fo digitado pelo usuário-->
+    
+    <ul class="lista-fotos">
+      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
+      
+        <meu-painel :titulo="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+        </meu-painel>
+        
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+
+import Painel from './components/shared/painel/Painel.vue';
+
 export default {
+
+  components: {
+    'meu-painel' : Painel//as aspas foram devido ao hífem no nome da "variavel"
+  },
+
   data() {
     return {
       titulo: "AluraPic",
 
-      fotos: []
+      fotos: [],
+
+      filtro: ''
     }
+  },
+
+  computed: {
+    fotosComFiltro() {
+      if(this.filtro){
+        //Cira uma expressão com o alor do filtro insesitivo
+        let exp = new RegExp(this.filtro.trim(),'i');
+        //retorna apenas as fotos que condizem com a expressão no filtro
+        return this.fotos.filter(foto => exp.test(foto.titulo));//esse filter() é uma função prórpia do javascript
+      }else{  
+        //se não tiver nada no filtro mostra todos os elementos
+        return this.fotos;
+      }
+    } 
   },
 
   created() {
@@ -29,5 +62,31 @@ export default {
 </script>
 
 <style>
+  .corpo{
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado{
+    text-align: center;
+  }
+
+  .lista-fotos{
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item{
+    display: inline-block;
+  }
+
+  .imagem-responsiva{
+    width: 100%;
+  }
+
+  .filtro{
+    display: block;
+    width: 100%;
+  }
 
 </style>
---
+++ alurapic/src/components/shared/painel/Painel.vue
@@ -0,0 +1,76 @@
+
+<template>
+
+        <div class="painel">
+
+            <h2 class="painel-titulo" @dblclick="visivel = !visivel">{{ titulo }}</h2><!--@ = v-on-->
+            <transition name="painel-fade"> <!--o transition só pode ter um elemento filho-->
+                <div class="painel-conteudo"  v-show="visivel"> <!--Para adicionar a directiva v-show é necessário que seja dentro de uma div,
+                pois se tentar colocar no slot não vai dar certo pois ele não aceita-->
+                    <slot> <!--o slot serve para indicar que a tag "meu-painel" deve manter 
+                    o conteúdo que ficar entre a abertura e fechamento dessa tag, nesse caso o img, 
+                    dentro da tag slot-->
+                
+                    </slot>
+                </div>
+            </transition>
+
+        </div>
+      
+</template>
+
+<script>
+
+export default {
+  
+    props: ['titulo'],//Ta indicando que pode receber um titulo
+
+    data() {
+        return{
+
+            visivel: true
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+    /* O scoped serve para indicar que eu quero que esse style só afete os elementos dentro 
+    desse componente Painel.vue */
+    
+    /* Estilo do Painel */
+    .painel {
+        padding: 0 auto;
+        border: solid 2px grey;
+        display: inline-block;
+        margin: 5px;
+        box-shadow: 5px 5px 10px grey;
+        width: 200px;
+        height: 100%;
+        vertical-align: top;
+        text-align: center;
+    }
+
+    .painel .painel-titulo {
+        text-align: center;
+        border: solid 2px;
+        background: lightblue;
+        margin: 0 0 15px 0;
+        padding: 10px;
+        text-transform: uppercase;
+    }
+
+    *{
+        box-shadow: 5px 5px 5px;
+    }
+
+    .painel-fade-enter, .painel-fade-leave-active {
+        opacity: 0;
+    }
+
+    .painel-fade-enter-active, .painel-fade-leave-active {
+        transition: opacity .4s;
+    }
+
+</style>
