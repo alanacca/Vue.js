000fa378764715d7895da7ff8976e0ec31c95759
alanacca <alana.araujo35@gmail.com>
2021-08-31 13:17:10 -0300

    Adicionando formulario sem estilização

--- alurapic/src/components/cadastro/Cadastro.vue
+++ alurapic/src/components/cadastro/Cadastro.vue
@@ -1,5 +1,36 @@
 <template>
-    <h2 class="centralizado"> Cadastro </h2>
+    <div>
+        <h1 class="centralizado"> Cadastro </h1>
+        <h2 class="centralizado"></h2>
+
+        <form>
+            <div class="controle">
+                <label for="titulo">Titulo</label>
+                <input id="titulo" autocomplete="off">
+            </div>
+
+            <div class="controle">
+                <label for="url">URL</label>
+                <input id="url" autocomplete="off">
+                <imagem-reponsiva/>
+            </div>
+
+            <div class="controle">
+                <label for="descriçãp">DESCRIÇÃO</label>
+                <textarea id="titulo" autocomplete="off"></textarea>
+            </div>
+
+            <div class="centralizado">
+                <meu-botao rotulo = "GRAVAR" tipo ="submit"/>
+                <router-link to="/"><meu-botao rotulo= "VOLTAR" type = "button" for="titulo"/></router-link>
+            </div>
+
+        </form>
+    
+    
+    
+    
+    </div>
 </template>
 
 <script>
